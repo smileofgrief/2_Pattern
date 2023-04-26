ca26f555ca4f95070812ea4f628e2caad1ae9935
Marina Kuchmasova <kuchmasova.m@gmail.com>
2023-04-26 09:06:26 +0300

    Initial commit

---
+++ src/test/java/ru/netology/testmode/data/UserRegistrationData.java
@@ -0,0 +1,12 @@
+package ru.netology.testmode.data;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class UserRegistrationData {
+    private final String login;
+    private final String password;
+    private final String status;
+}
